9fc2038c11a000a00152d6c72fb377bd2d288680
Williams, Dartanion De Angelo <dwilliams@bootcampspot.com>
2020-11-21 09:45:47 -0500

    Adding Unit 9.3 materials

---
+++ 01-Class-Activities/09-SQL/3/Activities/10-Stu_View_Room_Queries/Unsolved/query.sql
@@ -0,0 +1,7 @@
+-- Create the subquery
+
+-- Retrieve the same resuls using a join (hint: use INNER JOIN and GROUP BY)
+
+-- Create View (create the view using either the subquery or join approach)
+
+-- Query the view to the titles with 7 copies
