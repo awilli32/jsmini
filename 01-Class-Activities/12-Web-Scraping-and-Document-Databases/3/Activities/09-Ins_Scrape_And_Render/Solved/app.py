e5b013ba0a0b5abafa182879409c37b3ee69e28c
Williams, Dartanion De Angelo <dwilliams@bootcampspot.com>
2020-12-23 14:12:36 -0500

    Adding Unit 12.3 solutions

--- 01-Class-Activities/12-Web-Scraping-and-Document-Databases/3/Activities/09-Ins_Scrape_And_Render/Solved/app.py
+++ 01-Class-Activities/12-Web-Scraping-and-Document-Databases/3/Activities/09-Ins_Scrape_And_Render/Solved/app.py
@@ -1,30 +1,23 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+import pymongo
 import scrape_craigslist
-
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
-
+conn = 'mongodb://localhost:27017/craigslist_app'
+client = pymongo.MongoClient(conn)
+db = client.craigslist_app
 
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
+    listings = db.listings.find_one()
     return render_template("index.html", listings=listings)
-
 
 @app.route("/scrape")
 def scraper():
-    listings = mongo.db.listings
+    listings = db.listings
     listings_data = scrape_craigslist.scrape()
     listings.update({}, listings_data, upsert=True)
     return redirect("/", code=302)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
