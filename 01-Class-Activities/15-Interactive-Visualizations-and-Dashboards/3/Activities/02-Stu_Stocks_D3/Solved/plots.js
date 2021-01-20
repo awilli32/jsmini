35b08147bb4130adeaa31facfb5a634756fcbb2c
Dartanion Williams <dartanion@dartanion.net>
2021-01-19 23:43:39 -0500

    Adding Unit 15.3 solutions

---
+++ 01-Class-Activities/15-Interactive-Visualizations-and-Dashboards/3/Activities/02-Stu_Stocks_D3/Solved/plots.js
@@ -0,0 +1,69 @@
+var apiKey = "CBivvBn54csU-rbfu-KG";
+var startDate = '2017-10-01'
+var endDate = '2018-04-01'
+var stockSymbol = 'AMZN'
+
+/* global Plotly */
+var url =
+  `https://www.quandl.com/api/v3/datasets/WIKI/${stockSymbol}.json?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`;
+
+/**
+ * Helper function to select stock data
+ * Returns an array of values
+ * @param {array} rows
+ * @param {integer} index
+ * index 0 - Date
+ * index 1 - Open
+ * index 2 - High
+ * index 3 - Low
+ * index 4 - Close
+ * index 5 - Volume
+ */
+function unpack(rows, index) {
+  return rows.map(function(row) {
+    return row[index];
+  });
+}
+
+function buildPlot() {
+  d3.json(url).then(function(data) {
+
+    // Grab values from the data json object to build the plots
+    var name = data.dataset.name;
+    var stock = data.dataset.dataset_code;
+    var startDate = data.dataset.start_date;
+    var endDate = data.dataset.end_date;
+    var dates = unpack(data.dataset.data, 0);
+    var closingPrices = unpack(data.dataset.data, 4);
+
+    var trace1 = {
+      type: "scatter",
+      mode: "lines",
+      name: name,
+      x: dates,
+      y: closingPrices,
+      line: {
+        color: "#17BECF"
+      }
+    };
+
+    var data = [trace1];
+
+    var layout = {
+      title: `${stock} closing prices`,
+      xaxis: {
+        range: [startDate, endDate],
+        type: "date"
+      },
+      yaxis: {
+        autorange: true,
+        type: "linear"
+      }
+    };
+
+    Plotly.newPlot("plot", data, layout);
+
+  });
+}
+
+buildPlot();
